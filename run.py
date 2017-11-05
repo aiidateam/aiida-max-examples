ec84a325fdf1bff4e915c5869c70b5d91d9a5e0d
Leopold Talirz <leopold.talirz@gmail.com>
2017-11-05 11:26:07 +0100

    first commit

---
+++ run.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env runaiida
+"""Run aiida examples
+
+usage: ./run.py -c <aiida-code> -s <structure-label>
+"""
+from config import group_name
+
+
+def load_example_structures():
+    """ Read input structures into the database
+
+    Structures are read from subfolder "example-structures"
+    and stored in the group "example-structures".
+
+    :return: group of available structures
+    """
+    from aiida.orm.group import Group
+    
+    group, created = Group.get_or_create(name=group_name)
+    
+    if created:
+        import glob
+        from ase.io import read
+        from aiida.orm.data.structure import StructureData
+
+        filenames = glob.glob(group_name + '/*.cif')
+
+        for name in ['TiO2']:
+            structure = StructureData(ase=read(name + '.cif'))
+            structure.label = name
+            print("Storing {} in database".format(name))
+            structure.store()
+            group.add_nodes([structure])
+    
+        group.description = "\
+        Set of atomic structures used by examples for AiiDA plugins of different codes"
+        group.store()
+    
+    return group
+
+
+
+def parser_setup():
+    """Set up parser for command line arguments.
+    """
+    import argparse
+    parser = argparse.ArgumentParser(
+        description='Run an example structure with a specified code',
+    )
+
+    parser.add_argument(
+        '-c', type=str, required=True, dest='codename',
+        help='name of AiiDA code to run'
+    )
+
+    group = load_example_structures()
+    available_structures = ", ".join([n.label for n in group.nodes])
+    parser.add_argument(
+        '-s', default='TiO2', dest='structure',
+        help='The structure to run. Available: {}'.format(available_structures),
+    )
+
+    parser.add_argument(
+        '-k', nargs=3, type=int, default=[4, 4, 4], dest='kpoints', metavar='NK',
+        help='define the k-point mesh. (default: %(default)s)'
+    )
+
+    #parser.add_argument(
+    #    '-p', type=str, required=True, dest='pseudo_family',
+    #    help='the name of pseudo family to use'
+    #)
+
+    parser.add_argument(
+        '-w', type=int, default=1800, dest='max_wallclock_seconds', metavar='SEC',
+        help='maximum wallclock time in seconds (default: %(default)d)'
+    )
+
+    return parser
+
+
+def execute(args):
+    """Check arguments and run code
+
+    Determines which aiida plugin to use from the code specified.
+    """
+    from aiida.work.run import run
+    from aiida.common.exceptions import NotExistent
+    from aiida.orm.data.base import Int, Float, Str
+    StructureData = DataFactory('structure')
+    ParameterData = DataFactory('parameter')
+    KpointsData = DataFactory('array.kpoints')
+
+    # Get structure
+    group = load_example_structures()
+    labels = [ n.label for n in group.nodes]
+    nodes = [ n for n in group.nodes]
+    try:
+        structure = nodes[ labels.index(args.structure) ]
+    except ValueError:
+        raise ValueError("Structure {} not found. Available structures: {}"\
+            .format(args.structure, labels.join(", ")))
+    if not isinstance(structure, StructureData):
+        raise ValueError("Node {} is not of type StructureData".format(structure.pk))
+    print("Running example '{}'".format(args.structure))
+
+    # Get k-points
+    kpoints = KpointsData()
+    kpoints.set_kpoints_mesh(args.kpoints)
+
+    bandskpoints = KpointsData()
+    bandskpoints.set_cell(structure.cell, structure.pbc)
+    # bandskpoints.set_kpoints_path(kpoint_distance = 0.05)
+    bandskpoints.set_kpoints_path([
+        ('G', 'X', 21),
+        ('X', 'R', 31),
+        ('R', 'Z', 21),
+        ('Z', 'G', 31),
+        ('G', 'M', 31),
+        ('M', 'A', 31),
+        ('A', 'Z', 31),
+    ])
+
+    # options
+    options  = {
+        'resources': {
+            'num_machines': 1,
+            "num_mpiprocs_per_machine": 1,
+        },
+        'max_wallclock_seconds': args.max_wallclock_seconds,
+    }
+
+    # Get aiida code
+    code = Code.get_from_string(args.codename)
+    print("Running code '{}'".format(args.codename))
+
+    # Determine aiida plugin
+    input_plugin = code.get_input_plugin_name()
+    if 'siesta' in input_plugin:
+        from siesta_input import workchain, pseudo_family, parameters, settings, basis
+    elif 'fleur' in input_plugin:
+        from fleur import workchain, pseudo_family, parameters, settings, basis
+
+    results = run(
+        # general inputs
+        workchain,
+        code=code,
+        structure=structure,
+        kpoints=kpoints,
+        bandskpoints=bandskpoints,
+        # plugin-specific inputs
+        pseudo_family=Str(pseudo_family),
+        parameters=ParameterData(dict=parameters),
+        settings=ParameterData(dict=settings),
+        options=ParameterData(dict=options),
+        basis=ParameterData(dict=basis),
+        #max_iterations=Int(args.max_iterations),
+    )
+
+    return result
+
+
+
+def cli():
+    """Run command line interface
+    """
+    parser = parser_setup()
+    args   = parser.parse_args()
+    result = execute(args)
+
+
+if __name__ == "__main__":
+    cli()
---
+++ siesta_input.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env runaiida
+# -*- coding: utf-8 -*-
+from aiida.orm import Code, DataFactory, CalculationFactory
+from aiida_siesta.workflows.base import SiestaBaseWorkChain as workchain
+import aiida_siesta.data.psf as psf
+
+PsfData = DataFactory('siesta.psf')
+
+block_pao_basis_content = """
+Ti    5      1.91
+n=3    0    1   E     93.95      5.20
+5.69946662616249
+1.00000000000000
+n=3    1    1   E     95.47      5.20
+5.69941339465994
+1.00000000000000
+n=4    0    2   E     96.47      5.60
+6.09996398975307        5.09944363262274
+1.00000000000000        1.00000000000000
+n=3    2    2   E     46.05      4.95
+5.94327035784617        4.70009988294302
+1.00000000000000        1.00000000000000
+n=4    1    1   E      0.50      1.77
+3.05365979938936
+1.00000000000000
+O     3     -0.28
+n=2    0    2   E     40.58      3.95
+4.95272270428712        3.60331408800389
+1.00000000000000        1.00000000000000
+n=2    1    2   E     36.78      4.35
+4.99990228025066        3.89745395068600
+1.00000000000000        1.00000000000000
+n=3    2    1   E     21.69      0.93
+2.73276990670788
+1.00000000000000
+"""
+
+parameters = {
+    'xc-functional': 'GGA',
+    'xc-authors': 'PBE',
+    'spin-polarized': False,
+    'meshcutoff': '200 Ry',
+    'dm-numberpulay': 4,
+    'dm-mixingweight': 0.3,
+    'dm-tolerance': 1.e-4,
+    'electronic-temperature': '25 meV',
+    'max-scfiterations': 10,
+    'scf-must-converge': True,
+    'geometry-must-converge': True,
+    'Solution-method': 'diagon',
+    'md-maxcgsteps': 8,
+    'md-maxcgdispl': '0.1 Ang',
+    'md-maxforcetol': '0.02 eV/Ang',
+    'writeforces': True,
+    'writecoorstep': True,
+    'xml:write': True,
+}
+
+# default basis
+basis = {
+    # 'pao-energy-shift': '100 meV',
+    # '%block pao-basis-sizes': """
+    # Si DZP                    """,
+    '%block pao-basis': block_pao_basis_content,
+}
+
+settings = {}
+
+
+
+# upload pseudos
+folder = './siesta-files'
+pseudo_family = 'example-siesta-pps'
+pseudo_family_desc = "Siesta Pseudopotentials for example structures"
+
+files_found, files_uploaded = psf.upload_psf_family(
+    folder, pseudo_family, pseudo_family_desc, stop_if_existing=False)
+
+print("PSF files found: {}. New files uploaded: {}").format(
+    files_found, files_uploaded)
